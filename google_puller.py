a54bcb2744c469fe740e10456fab0affe533a61b
Amber Adams <amberdadams1@gmail.com>
2020-09-10 11:10:42 -0600

    add image saving functionality

--- google_puller.py
+++ google_puller.py
@@ -5,15 +5,16 @@ Created on Thu Sep 10 10:12:09 2020
 @author: Amber
 """
 
+import requests
+import logging
+import sys
 from selenium.webdriver import Firefox
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 class Puller():
-    def __init__(self):
+    def __init__(self, log_level):
         self.base_url = "https://www.google.com/search?tbm=isch&q="
         self.driver = Firefox()
+        
     
     def kill(self):
         self.driver.quit()
@@ -23,3 +24,8 @@ class Puller():
     
     def pull(self, index):
         result = self.driver.find_elements_by_class_name("rg_i")[index]
+        self.image = requests.get(result.get_attribute("src"))
+        
+    def save_pulled(self, save_path):
+        with file as open(save_path, "wb"):
+            file.write(response.content)
