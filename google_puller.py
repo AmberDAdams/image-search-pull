c39046b069f016a712ea03229b42b72bb1dcc560
Amber Adams <amberdadams1@gmail.com>
2020-09-10 23:07:15 -0600

    Complete logging

--- google_puller.py
+++ google_puller.py
@@ -31,13 +31,15 @@ class Puller():
         logging.basicConfig(stream=LOG_STREAM, level=LOG_LEVEL_DEFAULT, 
                                 format=LOG_FORMAT)
         self.logger = logging.getLogger(LOG_NAME)
+        self.logger.info("Puller instance created.")
     
     def kill(self):
         self.driver.quit()
-        del self
+        self.logger.info("Driver exited, feel free to delete puller instance")
         
     def search(self, search_term):
         self.driver.get(self.base_url + search_term)
+        self.logger.info("Images results page for '%s' loaded", search_term)
     
     def pull(self, index):
         result = self.driver.find_elements_by_class_name("isv-r")[index]
@@ -46,12 +48,12 @@ class Puller():
         time.sleep(random.triangular(0.3, 0.6, 0.4))
         url = self.driver.find_element_by_class_name("c1AlVc").get_attribute("href")
         self.driver.get(url)
-        print("loaded")
         url = ""
         while not url.startswith("http"):
             time.sleep(random.triangular(0.5, 1, 0.7))
             url = self.driver.find_element_by_id("imi").get_attribute("src")
         self.image = requests.get(url).content
+        self.logger.info("Image pulled from url at %s", url)
         
     def get_index_by_size(self, min_width=None, min_height=None,
                           max_width=None, max_height=None):
@@ -64,27 +66,24 @@ class Puller():
             img_height = int(img.get_attribute("data-oh"))
             # using info level for now because debug level is saturated
             # debug level is used for many of the modules used here
-            self.logger.info("Image Index #%s wxh %sx%s",
-                        cur_index, img_width, img_height)
             if min_width is not None and img_width < min_width:
-                self.logger.info("img width too small")
                 cur_index += 1
             elif max_width is not None and img_width > max_width:
-                self.logger.info("img width too large")
                 cur_index += 1
             elif min_height is not None and img_height < min_height:
-                self.logger.info("img height too small")
                 cur_index += 1
             elif max_height is not None and img_height > min_height:
-                self.logger.info("img height too large")
                 cur_index += 1
             else:
                 satisfied = True
+                self.logger.info("Using image Index #%s, %spx by %spx",
+                                 cur_index, img_width, img_height)
         return cur_index
         
     def save_pulled(self, save_path):
         with open(save_path, "wb") as file:
             file.write(self.image)
+        self.logger.info("Image saved to %s", save_path)
     
     def search_save(self, search_term, save_path, min_width=None,
                     min_height=None, max_width=None, max_height=None):
@@ -92,4 +91,3 @@ class Puller():
         index = self.get_index_by_size(min_width, min_height, max_width, max_height)
         self.pull(index)
         self.save_pulled(save_path)
-
